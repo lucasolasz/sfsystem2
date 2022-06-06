82626846d8efee419f8b859d367fbb7082153a35
lucasolasz <olaszlucas072@gmail.com>
2022-06-06 17:28:32 -0300

    Pagina home

--- app/Views/paginas/home.php
+++ app/Views/paginas/home.php
@@ -1,2 +1,8 @@
-<h1><?php echo $dados['tituloPagina'] ?></h1>
-<p><?php echo $dados['descricao'] ?></p>
+<div class="container p-5">
+    <div class="row">
+        <div class="col-md-8">
+            <h1 class="my-5"><?php echo APP_NOME ?></h1>
+            <p>HOME</p>
+        </div>
+    </div>
+</div>
