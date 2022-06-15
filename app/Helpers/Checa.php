2c9c25849aad334e788175515f8d0188c4e427f6
lucasolasz <olaszlucas072@gmail.com>
2022-06-14 22:55:38 -0300

    helpers de checagem

---
+++ app/Helpers/Checa.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Checa {
+
+
+    //Regex para receber apenas letras
+    public static function checarNome($nome) {
+        if(!preg_match('/^([áÁàÀãÃâÂéÉèÈêÊíÍìÌóÓòÒõÕôÔúÚùÙçÇaA-zZ]+)+((\s[áÁàÀãÃâÂéÉèÈêÊíÍìÌóÓòÒõÕôÔúÚùÙçÇaA-zZ]+)+)?$/',$nome)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public static function checarEmail($email){
+        if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+}
+
+
