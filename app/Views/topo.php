a04164c17c07de81b4e9b5c57cf566bcd8e2fa80
lucasolasz <olaszlucas072@gmail.com>
2022-06-16 17:13:14 -0300

    Adiciona permissões para acesso a menus

--- app/Views/topo.php
+++ app/Views/topo.php
@@ -9,24 +9,43 @@
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="li-a-artcor nav-link" aria-current="page" href="<?php echo URL ?>">Home</a>
-                        </li>
+                        <?php if (isset($_SESSION['id_usuario'])) {
+                            if ($_SESSION['fk_tipo_usuario'] == 1) { ?>
+                                
+                                <li class="nav-item">
+                                    <a class="li-a-artcor nav-link" aria-current="page" href="<?php echo URL ?>">Home</a>
+                                </li>
+
+                                <li class="nav-item dropdown">
+
+                                    <a class="li-a-artcor nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Cadastro</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-artcor nav-link" aria-current="page" href="<?php echo URL . '/usuariosController/cadastrar' ?>">Usuário</a></li>
+                                    </ul>
+                                </li>
+
+                                <li class="nav-item dropdown">
+
+                                    <a class="li-a-artcor nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Encontros literários</a>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-artcor nav-link" aria-current="page" href="<?php echo URL . '/agendaController' ?>">Agenda Convidados</a></li>
+                                    </ul>
+                                </li>
+                        <?php }
+                        } ?>
+
                         <li class="nav-item">
                             <a class="nav-link li-a-artcor" href="<?php echo URL . '/paginas/sobre' ?>">Sobre nós</a>
                         </li>
                     </ul>
 
-
-
                     <?php if (isset($_SESSION['id_usuario'])) { ?>
                         <span class="navbar-text">
-                            <p style="color: white;">Olá, <?php echo $_SESSION['ds_nome']; ?>, Seja bem vindo(a)</p>
+                            <p style="color: white;">Olá, <?php echo ucfirst($_SESSION['ds_nome_usuario']); ?>, Seja bem vindo(a)</p>
                             <a class="btn btn-sm btn-danger" href="<?php echo URL . '/usuariosController/sair' ?>">Sair</a>
                         </span>
                     <?php } else { ?>
                         <span class="navbar-text">
-                            <a href="<?php echo URL . '/usuariosController/cadastrar' ?>" class="btn btn-artcor">Cadastre-se</a>
                             <a href="<?php echo URL . '/usuariosController/login' ?>" class="btn btn-artcor">Entrar</a>
                         </span>
                     <?php } ?>
