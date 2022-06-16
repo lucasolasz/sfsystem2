9f07a85bed7b4bce4da64925450657162cb77929
lucasolasz <olaszlucas072@gmail.com>
2022-06-16 17:12:27 -0300

    Inclui icones bootstrap

--- public/index.php
+++ public/index.php
@@ -14,6 +14,7 @@ include './../app/autoload.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <link href="<?php echo URL ?>/public/css/estilos.css" rel="stylesheet">
     <link href='http://fonts.googleapis.com/css?family=Oswald:400,300,700' rel='stylesheet' type='text/css'>
     <title><?php echo APP_NOME ?></title>
