871a85e8fcffd25991b1d99bd904b2b391d1f3ad
lucasolasz <olaszlucas072@gmail.com>
2022-06-16 13:30:21 -0300

    Atualiza o sobre

--- app/Views/paginas/sobre.php
+++ app/Views/paginas/sobre.php
@@ -2,8 +2,7 @@
     <div class="row">
         <div class="col-md-8">
             <h1><?php echo $dados['tituloPagina'] ?></h3>
-            <p>Software PDV com controle de estoque e fluxo de caixa. 
-                Oferece suporte a relatórios e emissão de NFe's.
+            <p>O Portal ARTECULT acredita no poder transformador da arte e do conhecimento. Atualmente, além de vários serviços de divulgação para artistas e escritores, o site conta com vários canais de arte e conhecimento, contando com inúmeros colaboradores em cada segmento que nos ajudam a criar um um ambiente de celebração – e divulgação – de arte e cultura. Um ambiente que combate de forma bem particular toda ignorância e intolerância.
             </p>
         </div>
     </div>
