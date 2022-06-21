8169f6f22b7e6670000ef19517a113db171bac89
lucasolasz <olaszlucas072@gmail.com>
2022-06-20 23:58:22 -0300

    Ajusta visualiza os eventos em tabela

--- app/Controllers/AgendaController.php
+++ app/Controllers/AgendaController.php
@@ -33,48 +33,68 @@ class AgendaController extends Controller
         $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if (isset($formulario)) {
 
+            //Define campos que estão vazios como NULL
+            if (empty($formulario['txtDataTreinamento'])) {
+                $dataTreinamentoNull = NULL;
+            } else {
+                $dataTreinamentoNull = trim($formulario['txtDataTreinamento']);
+            }
+            if (empty($formulario['txtHoraTreinamento'])) {
+                $horaTreinamentoNull = NULL;
+            } else {
+                $horaTreinamentoNull = trim($formulario['txtHoraTreinamento']);
+            }
+            if (empty($formulario['txtDataLive'])) {
+                $dataLiveNull = NULL;
+            } else {
+                $dataLiveNull = trim($formulario['txtDataLive']);
+            }
+            if (empty($formulario['txtHoraLive'])) {
+                $horaLiveNull = NULL;
+            } else {
+                $horaLiveNull = trim($formulario['txtHoraLive']);
+            }
+
+
             $dados = [
                 'txtNomeConvidado' => trim($formulario['txtNomeConvidado']),
                 'txtDataEntrevistaEscrita' => trim($formulario['txtDataEntrevistaEscrita']),
                 'txtLinkEntrevistaEscrita' => trim($formulario['txtLinkEntrevistaEscrita']),
-                'txtDataTreinamento' => trim($formulario['txtDataTreinamento']),
-                'txtHoraTreinamento' => trim($formulario['txtHoraTreinamento']),
-                'txtDataLive' => trim($formulario['txtDataLive']),
-                'txtHoraLive' => trim($formulario['txtHoraLive']),
+                'txtDataTreinamento' => $dataTreinamentoNull,
+                'txtHoraTreinamento' => $horaTreinamentoNull,
+                'txtDataLive' => $dataLiveNull,
+                'txtHoraLive' => $horaLiveNull,
                 'txtLinkLive' => trim($formulario['txtLinkLive']),
                 'id_usuario' => $_SESSION['id_usuario']
             ];
 
-            // var_dump($formulario);
+            // var_dump($dados);
+            // exit();
 
+            //Verifica se está vazio
+            if (empty($formulario['txtNomeConvidado'])) {
+                $dados['nome_erro'] = "Preencha o Nome do Convidado";
+            }
+            if (empty($formulario['txtDataEntrevistaEscrita'])) {
+                $dados['dataEntrevista_erro'] = "Preencha a Data da Entrevista";
+            }
 
-            // if (in_array("", $formulario)) {
+            //Invoca método estatico da classe 
+            if (Checa::checarNome($formulario['txtNomeConvidado'])) {
+                $dados['nome_erro'] = "Nome inválido";
+            } elseif (Checa::checarData($formulario['txtDataEntrevistaEscrita'])) {
+                $dados['dataEntrevista_erro'] = "Data da Entrevista inválida";
+            } else {
 
-                //Verifica se está vazio
-                if (empty($formulario['txtNomeConvidado'])) {
-                    $dados['nome_erro'] = "Preencha o Nome do Convidado";
-                }
-                if (empty($formulario['txtDataEntrevistaEscrita'])) {
-                    $dados['dataEntrevista_erro'] = "Preencha a Data da Entrevista";
-                }
-            // } else {
-                //Invoca método estatico da classe 
-                if (Checa::checarNome($formulario['txtNomeConvidado'])) {
-                    $dados['nome_erro'] = "Nome inválido";
-                } elseif (Checa::checarData($formulario['txtDataEntrevistaEscrita'])) {
-                    $dados['dataEntrevista_erro'] = "Data da Entrevista inválida";
+                if ($this->agendaModel->armazenarAgenda($dados)) {
+
+                    //Para exibir mensagem success , não precisa informar o tipo de classe
+                    Alertas::mensagem('agenda', 'Evento cadastrado com sucesso');
+                    Redirecionamento::redirecionar('agendaController/index');
                 } else {
-
-                    if ($this->agendaModel->armazenarAgenda($dados)) {
-
-                        //Para exibir mensagem success , não precisa informar o tipo de classe
-                        Alertas::mensagem('agenda', 'Evento cadastrado com sucesso');
-                        Redirecionamento::redirecionar('agendaController/index');
-                    } else {
-                        die("Erro ao armazenar agenda no banco de dados");
-                    }
+                    die("Erro ao armazenar agenda no banco de dados");
                 }
-            // }
+            }
         } else {
             $dados = [
                 'txtNomeConvidado' => '',
@@ -105,14 +125,36 @@ class AgendaController extends Controller
         $formulario = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
         if (isset($formulario)) {
 
+            //Define campos que estão vazios como NULL
+            if (empty($formulario['txtDataTreinamento'])) {
+                $dataTreinamentoNull = NULL;
+            } else {
+                $dataTreinamentoNull = trim($formulario['txtDataTreinamento']);
+            }
+            if (empty($formulario['txtHoraTreinamento'])) {
+                $horaTreinamentoNull = NULL;
+            } else {
+                $horaTreinamentoNull = trim($formulario['txtHoraTreinamento']);
+            }
+            if (empty($formulario['txtDataLive'])) {
+                $dataLiveNull = NULL;
+            } else {
+                $dataLiveNull = trim($formulario['txtDataLive']);
+            }
+            if (empty($formulario['txtHoraLive'])) {
+                $horaLiveNull = NULL;
+            } else {
+                $horaLiveNull = trim($formulario['txtHoraLive']);
+            }
+
             $dados = [
                 'txtNomeConvidado' => trim($formulario['txtNomeConvidado']),
                 'txtDataEntrevistaEscrita' => trim($formulario['txtDataEntrevistaEscrita']),
                 'txtLinkEntrevistaEscrita' => trim($formulario['txtLinkEntrevistaEscrita']),
-                'txtDataTreinamento' => trim($formulario['txtDataTreinamento']),
-                'txtHoraTreinamento' => trim($formulario['txtHoraTreinamento']),
-                'txtDataLive' => trim($formulario['txtDataLive']),
-                'txtHoraLive' => trim($formulario['txtHoraLive']),
+                'txtDataTreinamento' => $dataTreinamentoNull,
+                'txtHoraTreinamento' => $horaTreinamentoNull,
+                'txtDataLive' => $dataLiveNull,
+                'txtHoraLive' =>  $horaLiveNull,
                 'txtLinkLive' => trim($formulario['txtLinkLive']),
                 'id_usuario' => $_SESSION['id_usuario'],
                 'id_agenda_convidados' => $id,
@@ -123,34 +165,29 @@ class AgendaController extends Controller
 
             // var_dump($dados);
 
+            //Verifica se está vazio
+            if (empty($formulario['txtNomeConvidado'])) {
+                $dados['nome_erro'] = "Preencha o Nome do Convidado";
+            }
+            if (empty($formulario['txtDataEntrevistaEscrita'])) {
+                $dados['dataEntrevista_erro'] = "Preencha a Data da Entrevista";
+            }
+            //Invoca método estatico da classe 
+            if (Checa::checarNome($formulario['txtNomeConvidado'])) {
+                $dados['nome_erro'] = "Nome inválido";
+            } elseif (Checa::checarData($formulario['txtDataEntrevistaEscrita'])) {
+                $dados['dataEntrevista_erro'] = "Data da Entrevista inválida";
+            } else {
 
-            // if (in_array("", $formulario)) {
+                if ($this->agendaModel->atualizarAgenda($dados)) {
 
-                //Verifica se está vazio
-                if (empty($formulario['txtNomeConvidado'])) {
-                    $dados['nome_erro'] = "Preencha o Nome do Convidado";
-                }
-                if (empty($formulario['txtDataEntrevistaEscrita'])) {
-                    $dados['dataEntrevista_erro'] = "Preencha a Data da Entrevista";
-                }
-            // } else {
-                //Invoca método estatico da classe 
-                if (Checa::checarNome($formulario['txtNomeConvidado'])) {
-                    $dados['nome_erro'] = "Nome inválido";
-                } elseif (Checa::checarData($formulario['txtDataEntrevistaEscrita'])) {
-                    $dados['dataEntrevista_erro'] = "Data da Entrevista inválida";
+                    //Para exibir mensagem success , não precisa informar o tipo de classe
+                    Alertas::mensagem('agenda', 'Evento atualizado com sucesso');
+                    Redirecionamento::redirecionar('agendaController/index');
                 } else {
-
-                    if ($this->agendaModel->atualizarAgenda($dados)) {
-
-                        //Para exibir mensagem success , não precisa informar o tipo de classe
-                        Alertas::mensagem('agenda', 'Evento atualizado com sucesso');
-                        Redirecionamento::redirecionar('agendaController/index');
-                    } else {
-                        die("Erro ao atualizar a agenda no banco de dados");
-                    }
+                    die("Erro ao atualizar a agenda no banco de dados");
                 }
-            // }
+            }
         } else {
 
             $dados = [
@@ -186,15 +223,12 @@ class AgendaController extends Controller
                 //Para exibir mensagem success , não precisa informar o tipo de classe
                 Alertas::mensagem('agenda', 'Evento deletado com sucesso');
                 Redirecionamento::redirecionar('agendaController');
-
             } else {
                 die("Erro ao deletar o evento da agenda no banco de dados");
             }
-
         } else {
             Alertas::mensagem('agenda', 'Não foi possível deletar o evento', 'alert alert-danger');
             Redirecionamento::redirecionar('agendaController');
         }
-
     }
 }
--- app/Views/agendaConvidados/index.php
+++ app/Views/agendaConvidados/index.php
@@ -14,47 +14,74 @@
 
         </div>
         <div class="card-body">
-            <div class="row row-cols-1 row-cols-md-3 g-4">
-                <?php foreach ($dados['eventos'] as $eventos) { ?>
-                    <div class="col">
-                        <div class="card">
-                            <div class="card-header artcor">
-                                Convidado: <b style="color: white;"><?php echo $eventos->ds_nome_convidado ?></b>
+            <div class="table-responsive">
+                <table class="table table-hover">
+                    <thead>
+                        <tr>
+                            <th scope="col">Nome Convidado</th>
+                            <th scope="col">Data Entrevista Escrita</th>
+                            <th scope="col">Link Entrevista Escrita</th>
+                            <th scope="col">Data Treinamento</th>
+                            <th scope="col">Hora Treinamento</th>
+                            <th scope="col">Data Live</th>
+                            <th scope="col">Hora Live</th>
+                            <th scope="col">Link Live</th>
+                            <th scope="col">Ações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        // Exibe mensagem caso não tenha nenhum evento
+                        if (empty($dados['eventos'])) { ?>
 
-                                <div style="float: right;">
-                                    <ul class="list-inline">
-                                        <li class="list-inline-item">
-                                            <a href="<?php echo URL . '/agendaController/editar/' . $eventos->id_agenda_convidados ?>" class="btn btn-artcor"><i class="bi bi-pencil-square"></i></a>
-                                        </li>
-                                        <li class="list-inline-item">
-                                            <form action="<?php echo URL . '/agendaController/deletar/' . $eventos->id_agenda_convidados ?>" method="POST">
-                                                
-                                            <button type="submit" class="btn btn-danger"><span><i class="bi bi-trash-fill"></i></span></button>
-                                            </form>
-                                        </li>
-                                    </ul>
-                                </div>
-                            </div>
-                            <div class="card-body">
-
-                                <p>Data Entrevista Escrita: <?php echo Checa::dataBr($eventos->dt_entrevista_escrita) ?></p>
-                                <p>Link Entrevista Escrita: <a href="<?php echo $eventos->link_entrevista_escrita ?>" target="_blank" rel="noopener noreferrer">Link</a></p>
-                                <hr>
-                                <p>Data Treinamento: <?php echo Checa::dataBr($eventos->dt_treinamento) ?></p>
-                                <p>Hora Treinamento: <?php echo Checa::horaFormat($eventos->dt_hora_treinamento) ?></p>
-                                <hr>
-                                <p>Data Live: <?php echo Checa::dataBr($eventos->dt_live) ?></p>
-                                <p>Hora Live: <?php echo Checa::horaFormat($eventos->dt_hora_live) ?> </p>
-                                <p>Link Live: <a href="<?php echo $eventos->link_live ?>" target="_blank" rel="noopener noreferrer">Link</a></p>
+                            <tr>
+                                <td colspan="9" class="align-middle">Nenhum Evento cadastrado</td>
+                            </tr>
 
 
-                            </div>
-                            <div class="card-footer text-muted">
-                                <p>Criado por: <b><?php echo ucfirst($eventos->ds_nome_usuario) ?></b> em <i><?php echo Checa::dataBr($eventos->criado_em) ?></i></p>
-                            </div>
-                        </div>
-                    </div>
-                <?php  } ?>
+                        <?php  }
+
+
+                        foreach ($dados['eventos'] as $eventos) { ?>
+
+                            <tr>
+                                <td><?php echo $eventos->ds_nome_convidado ?></td>
+                                <td><?php echo Checa::dataBr($eventos->dt_entrevista_escrita) ?></td>
+
+                                <!-- Verifica se link está vazio e não exibir botão de link vazio -->
+                                <?php if (empty($eventos->link_entrevista_escrita)) { ?>
+                                    <td></td>
+                                <?php } else { ?>
+                                    <td><a href="<?php echo $eventos->link_entrevista_escrita ?>" target="_blank" rel="noopener noreferrer" class="btn btn-danger"><i class="bi bi-play-btn-fill"></i></a></td>
+                                <?php } ?>
+
+                                <td><?php echo Checa::dataBr($eventos->dt_treinamento) ?></td>
+                                <td><?php echo Checa::horaFormat($eventos->dt_hora_treinamento) ?></td>
+                                <td><?php echo Checa::dataBr($eventos->dt_live) ?></td>
+                                <td><?php echo Checa::horaFormat($eventos->dt_hora_live) ?> </p>
+
+                                <!-- Verifica se link está vazio e não exibir botão de link vazio -->
+                                <?php if (empty($eventos->link_live)) { ?>
+                                    <td></td>
+                                <?php } else { ?>
+                                <td><a href="<?php echo $eventos->link_live ?>" target="_blank" rel="noopener noreferrer" class="btn btn-danger"><i class="bi bi-play-btn-fill"></i></a></td>
+                            <?php } ?>
+
+                            <td><a href="<?php echo URL . '/agendaController/editar/' . $eventos->id_agenda_convidados ?>" class="btn btn-artcor"><i class="bi bi-pencil-square"></i></a></td>
+                            <td>
+                                <form action="<?php echo URL . '/agendaController/deletar/' . $eventos->id_agenda_convidados ?>" method="POST">
+                                    <button type="submit" class="btn btn-danger"><span><i class="bi bi-trash-fill"></i></span></button>
+                                </form>
+                            </td>
+
+
+
+                            <!-- <p>Criado por: <b><?php echo ucfirst($eventos->ds_nome_usuario) ?></b> em <i><?php echo Checa::dataBr($eventos->criado_em) ?></i></p> -->
+                            </tr>
+
+                        <?php  } ?>
+                    </tbody>
+                </table>
             </div>
         </div>
 
