d4706626c6a8e3df5984cf54dd40c4ec9e8c77e8
lucasolasz <olaszlucas072@gmail.com>
2024-11-03 11:57:33 -0300

    Remove a página sobre

--- app/controllers/Paginas.php
+++ app/controllers/Paginas.php
@@ -12,18 +12,4 @@ class Paginas extends Controller
         //Chamada do novo objeto PAGINAS 
         $this->view('paginas/home', $dados);
     }
-
-
-    public function sobre(){
-
-        //Parâmetros enviados para o método do controller VIEW
-        $dados = [
-            'tituloPagina' => 'Sobre nós'            
-        ];
-
-        //Chamada do novo objeto PAGINAS 
-        $this->view('paginas/sobre', $dados);
-
-        
-    }
 }
