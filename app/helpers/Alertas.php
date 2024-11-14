c1b88b0e2dcf9c80fcb0c25a1617feac8369574e
lucasolasz <olaszlucas072@gmail.com>
2024-11-13 23:34:22 -0300

    Botao para fechar mensagem de alerta

--- app/helpers/Alertas.php
+++ app/helpers/Alertas.php
@@ -18,7 +18,8 @@ class Alertas
                 $_SESSION[$nome . 'classe'] = $classe;
             } elseif (!empty($_SESSION[$nome]) && empty($texto)) {
                 $classe = !empty($_SESSION[$nome . 'classe']) ? $_SESSION[$nome . 'classe'] : 'alert alert-success';
-                echo '<div class="' . $classe . '">' . $_SESSION[$nome] . '</div>';
+                echo '<div class="' . $classe . ' alert-dismissible fade show ">' . $_SESSION[$nome] .
+                    '<button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button> </div>';
 
                 unset($_SESSION[$nome]);
                 unset($_SESSION[$nome . 'classe']);
