df45102d80f92e628651dc95b1354b9a38defc7f
lucasolasz <olaszlucas072@gmail.com>
2025-01-08 11:14:12 -0300

    Remove o tipo de usuário

--- app/views/usuarios/cadastrar.php
+++ app/views/usuarios/cadastrar.php
@@ -7,41 +7,29 @@
             <form name="cadastrar" method="POST" action="<?= URL . 'Usuarios/cadastrar' ?>">
                 <div class="mb-3">
                     <label for="txtNome" class="form-label">Nome: *</label>
-                    <input type="text" class="form-control <?= $dados['nome_erro'] ? 'is-invalid' : '' ?>" name="txtNome" id="txtNome" value="<?= $dados['txtNome'] ?>">
+                    <input type="text" class="form-control <?= $dados['nome_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtNome" id="txtNome" value="<?= $dados['txtNome'] ?>">
                     <!-- Div para exibir o erro abaixo do campo -->
                     <div class="invalid-feedback"><?= $dados['nome_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtSobreNome" class="form-label">Sobrenome: *</label>
-                    <input type="text" class="form-control <?= $dados['sobrenome_erro'] ? 'is-invalid' : '' ?>" name="txtSobreNome" id="txtSobreNome" value="<?= $dados['txtSobreNome'] ?>">
+                    <input type="text" class="form-control <?= $dados['sobrenome_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtSobreNome" id="txtSobreNome" value="<?= $dados['txtSobreNome'] ?>">
                     <!-- Div para exibir o erro abaixo do campo -->
                     <div class="invalid-feedback"><?= $dados['sobrenome_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtEmail" class="form-label">E-mail: *</label>
-                    <input type="text" class="form-control <?= $dados['email_erro'] ? 'is-invalid' : '' ?>" name="txtEmail" id="txtEmail" value="<?= $dados['txtEmail'] ?>">
+                    <input type="text" class="form-control <?= $dados['email_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtEmail" id="txtEmail" value="<?= $dados['txtEmail'] ?>">
                     <div class="invalid-feedback"><?= $dados['email_erro'] ?></div>
                 </div>
 
                 <div class="mb-3">
-                    <label for="txtEmail" class="form-label">Tipo usuário: *</label>
-                    <select class="form-select <?= $dados['tipoUsuario_erro'] ? 'is-invalid' : '' ?>" name="cboTipoUsuario" id="cboTipoUsuario">
-                        <option value="NULL"></option>
-                        <?php foreach ($dados['tiposUsuario'] as $tiposUsuario) {
-                            //Resgata valor do select 
-                            $tiposUsuarioSelected = '';
-                            if ($tiposUsuario->id_tipo_usuario == $dados['cboTipoUsuario']) {
-                                $tiposUsuarioSelected = 'selected';
-                            }
-                        ?>
-                            <option <?= $tiposUsuarioSelected ?> value="<?= $tiposUsuario->id_tipo_usuario ?>"><?= $tiposUsuario->ds_tipo_usuario ?></option>
-                        <?php } ?>
-                    </select>
-                    <div class="invalid-feedback"><?= $dados['tipoUsuario_erro'] ?></div>
-                </div>
-                <div class="mb-3">
                     <label for="txtEmail" class="form-label">Cargo usuário: *</label>
-                    <select class="form-select <?= $dados['tipoCargo_erro'] ? 'is-invalid' : '' ?>" name="cboCargoUsuario" id="cboCargoUsuario">
+                    <select class="form-select <?= $dados['tipoCargo_erro'] ? 'is-invalid' : '' ?>"
+                        name="cboCargoUsuario" id="cboCargoUsuario">
                         <option value="NULL"></option>
                         <?php foreach ($dados['cargoUsuario'] as $cargoUsuario) {
                             //Resgata valor do select 
@@ -49,21 +37,25 @@
                             if ($cargoUsuario->id_cargo == $dados['cboCargoUsuario']) {
                                 $cargoSelected = 'selected';
                             }
-                        ?>
-                            <option <?= $cargoSelected ?> value="<?= $cargoUsuario->id_cargo ?>"><?= $cargoUsuario->ds_cargo ?></option>
-                        <?php
+                            ?>
+                            <option <?= $cargoSelected ?> value="<?= $cargoUsuario->id_cargo ?>">
+                                <?= $cargoUsuario->ds_cargo ?>
+                            </option>
+                            <?php
                         } ?>
                     </select>
                     <div class="invalid-feedback"><?= $dados['tipoCargo_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtSenha" class="form-label">Senha: *</label>
-                    <input type="password" class="form-control <?= $dados['senha_erro'] ? 'is-invalid' : '' ?>" name="txtSenha" id="txtSenha" value="<?= $dados['txtSenha'] ?>">
+                    <input type="password" class="form-control <?= $dados['senha_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtSenha" id="txtSenha" value="<?= $dados['txtSenha'] ?>">
                     <div class="invalid-feedback"><?= $dados['senha_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtConfirmaSenha" class="form-label">Confirmar Senha: *</label>
-                    <input type="password" class="form-control <?= $dados['confirma_senha_erro'] ? 'is-invalid' : '' ?>" name="txtConfirmaSenha" id="txtConfirmaSenha">
+                    <input type="password" class="form-control <?= $dados['confirma_senha_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtConfirmaSenha" id="txtConfirmaSenha">
                     <div class="invalid-feedback"><?= $dados['confirma_senha_erro'] ?></div>
                 </div>
                 <div class="d-flex">
@@ -71,7 +63,8 @@
                         <input type="submit" value="Cadastrar" class="btn btn-primary">
                     </div>
                     <div class="p-2">
-                        <a class="btn btn-secondary" href="<?= URL . 'Usuarios/visualizarUsuarios' ?>" role="button">Cancelar</a>
+                        <a class="btn btn-secondary" href="<?= URL . 'Usuarios/visualizarUsuarios' ?>"
+                            role="button">Cancelar</a>
                     </div>
                 </div>
             </form>
--- app/views/usuarios/editar.php
+++ app/views/usuarios/editar.php
@@ -3,7 +3,8 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="<?= URL . 'Usuarios/visualizarUsuarios' ?>">Usuários</a></li>
-            <li class="breadcrumb-item active" aria-current="page"><?= ucfirst($dados['usuario']->ds_nome_usuario) ?></li>
+            <li class="breadcrumb-item active" aria-current="page"><?= ucfirst($dados['usuario']->ds_nome_usuario) ?>
+            </li>
         </ol>
     </nav>
 
@@ -12,44 +13,33 @@
             <h2>Editar Usuário</h2>
             <small>Preencha o formulário abaixo para editar o usuário</small>
 
-            <form name="editar" method="POST" action="<?= URL . 'Usuarios/editarUsuario/' . $dados['usuario']->id_usuario ?>">
+            <form name="editar" method="POST"
+                action="<?= URL . 'Usuarios/editarUsuario/' . $dados['usuario']->id_usuario ?>">
                 <div class="mb-3 mt-4">
                     <label for="txtNome" class="form-label">Nome: *</label>
-                    <input type="text" class="form-control <?= $dados['nome_erro'] ? 'is-invalid' : '' ?>" name="txtNome" id="txtNome" value="<?= $dados['usuario']->ds_nome_usuario ?>">
+                    <input type="text" class="form-control <?= $dados['nome_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtNome" id="txtNome" value="<?= $dados['usuario']->ds_nome_usuario ?>">
                     <!-- Div para exibir o erro abaixo do campo -->
                     <div class="invalid-feedback"><?= $dados['nome_erro'] ?></div>
                 </div>
                 <div class="mb-3 mt-4">
                     <label for="txtSobreNome" class="form-label">Sobrenome: *</label>
-                    <input type="text" class="form-control <?= $dados['sobrenome_erro'] ? 'is-invalid' : '' ?>" name="txtSobreNome" id="txtSobreNome" value="<?= $dados['usuario']->ds_sobrenome_usuario ?>">
+                    <input type="text" class="form-control <?= $dados['sobrenome_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtSobreNome" id="txtSobreNome" value="<?= $dados['usuario']->ds_sobrenome_usuario ?>">
                     <!-- Div para exibir o erro abaixo do campo -->
                     <div class="invalid-feedback"><?= $dados['sobrenome_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtEmail" class="form-label">E-mail: *</label>
-                    <input type="text" class="form-control <?= $dados['email_erro'] ? 'is-invalid' : '' ?>" name="txtEmail" id="txtEmail" value="<?= $dados['usuario']->ds_email_usuario ?>">
+                    <input type="text" class="form-control <?= $dados['email_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtEmail" id="txtEmail" value="<?= $dados['usuario']->ds_email_usuario ?>">
                     <div class="invalid-feedback"><?= $dados['email_erro'] ?></div>
                 </div>
 
                 <div class="mb-3">
-                    <label for="cboTipoUsuario" class="form-label">Tipo usuário: *</label>
-                    <select class="form-select <?= $dados['tipoUsuario_erro'] ? 'is-invalid' : '' ?>" name="cboTipoUsuario" id="cboTipoUsuario">
-                        <option value="NULL"></option>
-                        <?php foreach ($dados['tiposUsuario'] as $tiposUsuario) {
-                            //Resgata valor do select 
-                            $tiposUsuarioSelected = '';
-                            if ($tiposUsuario->id_tipo_usuario == $dados['usuario']->fk_tipo_usuario) {
-                                $tiposUsuarioSelected = 'selected';
-                            }
-                        ?>
-                            <option <?= $tiposUsuarioSelected ?> value="<?= $tiposUsuario->id_tipo_usuario ?>"><?= $tiposUsuario->ds_tipo_usuario ?></option>
-                        <?php } ?>
-                    </select>
-                    <div class="invalid-feedback"><?= $dados['tipoUsuario_erro'] ?></div>
-                </div>
-                <div class="mb-3">
                     <label for="cboCargoUsuario" class="form-label">Cargo usuário: *</label>
-                    <select class="form-select <?= $dados['tipoCargo_erro'] ? 'is-invalid' : '' ?>" name="cboCargoUsuario" id="cboCargoUsuario">
+                    <select class="form-select <?= $dados['tipoCargo_erro'] ? 'is-invalid' : '' ?>"
+                        name="cboCargoUsuario" id="cboCargoUsuario">
                         <option value="NULL"></option>
                         <?php foreach ($dados['cargoUsuario'] as $cargoUsuario) {
                             //Resgata valor do select 
@@ -57,21 +47,24 @@
                             if ($cargoUsuario->id_cargo == $dados['usuario']->fk_cargo) {
                                 $cargoSelected = 'selected';
                             }
-                        ?>
-                            <option <?= $cargoSelected ?> value="<?= $cargoUsuario->id_cargo ?>"><?= $cargoUsuario->ds_cargo ?></option>
-                        <?php
+                            ?>
+                            <option <?= $cargoSelected ?> value="<?= $cargoUsuario->id_cargo ?>">
+                                <?= $cargoUsuario->ds_cargo ?></option>
+                            <?php
                         } ?>
                     </select>
                     <div class="invalid-feedback"><?= $dados['tipoCargo_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtSenha" class="form-label">Senha: *</label>
-                    <input type="password" class="form-control <?= $dados['senha_erro'] ? 'is-invalid' : '' ?>" name="txtSenha" id="txtSenha" value="<?= $dados['txtSenha'] ?>">
+                    <input type="password" class="form-control <?= $dados['senha_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtSenha" id="txtSenha" value="<?= $dados['txtSenha'] ?>">
                     <div class="invalid-feedback"><?= $dados['senha_erro'] ?></div>
                 </div>
                 <div class="mb-3">
                     <label for="txtConfirmaSenha" class="form-label">Confirmar Senha: *</label>
-                    <input type="password" class="form-control <?= $dados['confirma_senha_erro'] ? 'is-invalid' : '' ?>" name="txtConfirmaSenha" id="txtConfirmaSenha">
+                    <input type="password" class="form-control <?= $dados['confirma_senha_erro'] ? 'is-invalid' : '' ?>"
+                        name="txtConfirmaSenha" id="txtConfirmaSenha">
                     <div class="invalid-feedback"><?= $dados['confirma_senha_erro'] ?></div>
                 </div>
                 <div class="d-flex">
@@ -79,7 +72,8 @@
                         <input type="submit" value="Salvar" class="btn btn-primary">
                     </div>
                     <div class="p-2">
-                        <a class="btn btn-secondary" href="<?= URL . 'Usuarios/visualizarUsuarios' ?>" role="button">Cancelar</a>
+                        <a class="btn btn-secondary" href="<?= URL . 'Usuarios/visualizarUsuarios' ?>"
+                            role="button">Cancelar</a>
                     </div>
                 </div>
             </form>
